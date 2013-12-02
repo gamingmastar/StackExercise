eba0315f6531b9cc402a911d7c6f79a13de56210
Thomas Ochman <thomas@nocebo.se>
2013-12-02 11:45:21 +0100

    New error: 'expected #<Stack:0x007f90b515b6a0> to respond to :push'

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe Stack do
+  describe "#initialize" do
+    it "create a new stack" do
+      Stack.new.should_not be_nil 
+    end
+  end
+  describe "#push" do
+    it "has a push method" do
+      Stack.new.should respond_to :push
+    end
+  end
 end
