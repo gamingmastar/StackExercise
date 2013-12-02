11bd0d20984577f2e5c1b14d93ff7eb11ea83a8d
Sam Joseph <tansaku@gmail.com>
2013-12-02 11:23:46 +0000

    refactoring step

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,33 +1,32 @@
 require 'spec_helper'
 
 describe Stack do
+  let(:stack) {Stack.new}
   describe "#initialize" do
     it "create a new stack" do
-      Stack.new.should_not be_nil 
+      stack.should_not be_nil 
     end
   end
   describe "#push" do
     it "has a push method" do
-      Stack.new.should respond_to :push
+      stack.should respond_to :push
     end
     it "allows an object to be pushed to it" do
-      Stack.new.push 1
+      stack.push 1
     end
   end
   describe "#pop" do
     it "has a pop method" do
-      Stack.new.should respond_to :pop
+      stack.should respond_to :pop
     end
     it "returns nil when the stack is empty" do
-      Stack.new.pop.should be_nil
+      stack.pop.should be_nil
     end
     it "returns the last item" do
-      stack = Stack.new
       stack.push 1
       stack.pop.should eq 1
     end
     it "returns previously pushed items - last in, first out (LIFO)" do
-      stack = Stack.new
       stack.push 2 
       stack.push "Hello"
       stack.push stack 
